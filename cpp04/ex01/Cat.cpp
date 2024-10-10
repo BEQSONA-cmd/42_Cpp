6c604ef8471361d5b107e93344c9f9afadd237f3
Beqa <tvildiani2001@gmail.com>
2024-10-10 19:23:36 +0200

    update

---
+++ cpp04/ex01/Cat.cpp
@@ -0,0 +1,37 @@
+#include "Cat.hpp"
+
+Cat::Cat()
+{
+    this->brain = new Brain();
+    this->type = "Cat";
+    std::cout << YELLOW << "Cat: Default constructor called" << RESET << std::endl;
+}
+
+Cat::Cat(const Cat &cat) : Animal(cat)
+{
+    this->operator=(cat);
+    std::cout << YELLOW << "Cat: Copy constructor called" << RESET << std::endl;
+}
+
+Cat &Cat::operator=(const Cat &cat)
+{
+    this->brain = new Brain(*cat.brain);
+    this->type = cat.type;
+    return *this;
+}
+
+Cat::~Cat()
+{
+    delete this->brain;
+    std::cout << RED << "Cat: Destructor called" << RESET << std::endl;
+}
+
+void Cat::makeSound() const
+{
+    std::cout << BLUE << "Cat: MEOW\n" << RESET << std::endl;
+}
+
+std::string Cat::getType() const
+{
+    return this->type;
+}
