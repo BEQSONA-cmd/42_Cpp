a42d38f284414b3b6619beff3cb743b7f67aa994
BEQSONA-cmd <tvildiani2001@gmail.com>
2024-10-11 16:04:01 +0200

    cpp04

---
+++ cpp04/ex02/main.cpp
@@ -0,0 +1,23 @@
+#include "Cat.hpp"
+#include "Dog.hpp"
+#include "WrongCat.hpp"
+#include "Brain.hpp"
+#include <vector>
+
+int main(void)
+{
+    // Animal *animal = new Animal();
+
+    // delete animal;
+
+    Animal *dog = new Dog();
+    Animal *cat = new Cat();
+
+    // makesound 
+
+    dog->makeSound();
+    cat->makeSound();
+
+    delete dog;
+    delete cat;
+}
