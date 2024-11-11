cbfbe6ed45d38a384f252fe0e2c93c841a98c546
Beqa <tvildiani2001@gmail.com>
2024-11-11 18:37:33 +0100

    cpp07

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -7,18 +7,42 @@
 #define CYAN "\033[1;36m"
 #define RESET "\033[0m"
 #include <iostream>
+#include <exception>
+#include <string>
+
+class Exception : public std::exception
+{
+    private:
+        std::string message;
+    public:
+        Exception(std::string message){ this->message = message; }
+        virtual ~Exception() throw() {}
+        virtual const char *what() const throw(){ return this->message.c_str(); }
+};
 
 template <typename T>
 class Array
 {
     private:
-        T *_array;
+        T *array;
         unsigned int _size;
     public:
         Array();
+
         Array(const Array &array);
         Array &operator=(const Array &array);
         ~Array();
+
+        Array(unsigned int n);
+        T &operator[](unsigned int iter);
+        unsigned int size() { return this->_size; }
+
+        Exception outOfBoundsException() { return Exception( RED "Out of bounds" RESET ); }
 };
+
+template <typename T>
+std::ostream &operator<<(std::ostream &out, const Array<T> &array);
+
+#include "Array.tpp"
 
 #endif
