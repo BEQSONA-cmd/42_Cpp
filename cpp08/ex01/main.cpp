fa799b58617e8d5a7b90b444df15411a0a66e88c
Beqa <tvildiani2001@gmail.com>
2024-11-13 18:09:48 +0100

    cpp07

---
+++ cpp08/ex01/main.cpp
@@ -0,0 +1,38 @@
+#include "Span.hpp"
+
+// int main()
+// {
+//     Span sp = Span(5);
+//     sp.addNumber(6);
+//     sp.addNumber(3);
+//     sp.addNumber(17);
+//     sp.addNumber(9);
+//     sp.addNumber(11);
+//     std::cout << sp.shortestSpan() << std::endl;
+//     std::cout << sp.longestSpan() << std::endl;
+//     return(0);
+// }
+
+void full_array(void)
+{
+    Span sp = Span(5);
+    try
+    {
+        sp.addNumber(6);
+        sp.addNumber(3);
+        sp.addNumber(17);
+        sp.addNumber(9);
+        sp.addNumber(11);
+        std::cout << "Shortest span: " << sp.shortestSpan() << std::endl;
+        std::cout << "Longest span: " << sp.longestSpan() << std::endl;
+    }
+    catch (std::exception &e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+}
+
+int main(void)
+{
+    return (0);
+}
