984065b7c922a1d80f31cf022836747fe86134e8
Beqa <tvildiani2001@gmail.com>
2024-11-27 19:29:14 +0100

    cpp09

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -20,3 +20,43 @@ void print(std::vector<int> nums)
     }
     std::cout << std::endl;
 }
+
+int jacobsthal(int n)
+{
+    if(n == 0)
+        return 0;
+    if(n == 1)
+        return 1;
+    return jacobsthal(n - 1) + 2 * jacobsthal(n - 2);
+}
+
+void print_pairs(std::vector<std::pair<int, int > > &pairs)
+{
+    for (size_t i = 0; i < pairs.size(); i++)
+        std::cout << "[" << pairs[i].first << ", " << pairs[i].second << "]" << std::endl;
+}
+
+void swap_pairs(std::vector<std::pair<int, int> > &pairs)
+{
+    size_t i = 0;
+    while (i < pairs.size())
+    {
+        if (pairs[i].second > pairs[i].first)
+            std::swap(pairs[i].second, pairs[i].first);
+        i++;
+    }
+}
+
+std::vector<std::pair<int, int> > create_pairs(std::vector<int> &nums)
+{
+    std::vector<std::pair<int, int> > pairs;
+
+    size_t i = 0;
+    while (i < nums.size())
+    {
+        if (i + 1 < nums.size())
+            pairs.push_back(std::make_pair(nums[i], nums[i + 1]));
+        i += 2;
+    }
+    return pairs;
+}
--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -1,14 +1,14 @@
 #include "PmergeMe.hpp"
 
-void merge_and_insert(std::vector<int> left, std::vector <int> right, std::vector<int> &nums)
+void merge_and_insert(std::vector<std::pair<int, int> > &left, std::vector<std::pair<int, int> > &right, std::vector<std::pair<int, int> > &nums)
 {
-    std::vector<int> result;
+    std::vector<std::pair<int, int> > result;
     size_t i = 0;
     size_t j = 0;
 
     while(i < left.size() && j < right.size())
     {
-        if(left[i] <= right[j])
+        if(left[i].first <= right[j].first)
         {
             result.push_back(left[i]);
             i++;
@@ -32,14 +32,14 @@ void merge_and_insert(std::vector<int> left, std::vector <int> right, std::vecto
     nums = result;
 }
 
-void PmergeMe(std::vector<int> &nums)
+void recursive_sort_larger_element(std::vector<std::pair<int, int> > &nums)
 {
     if(nums.size() <= 1)
         return;
     size_t i = 0;
 
-    std::vector<int> left;
-    std::vector<int> right;
+    std::vector<std::pair<int, int> > left;
+    std::vector<std::pair<int, int> > right;
     size_t mid = nums.size() / 2;
 
     while(i < mid)
@@ -52,14 +52,41 @@ void PmergeMe(std::vector<int> &nums)
         right.push_back(nums[i]);
         i++;
     }
-    PmergeMe(left);
-    PmergeMe(right);
+    recursive_sort_larger_element(left);
+    recursive_sort_larger_element(right);
 
     merge_and_insert(left, right, nums);
 }
 
+void PmergeMe(std::vector<int> &nums)
+{
+    if(nums.size() <= 1)
+        return;
+
+    bool has_stragler = false;
+    int stragler = 0;
+    if(nums.size() % 2 != 0)
+    {
+        has_stragler = true;
+        stragler = nums[nums.size() - 1];
+        nums.pop_back();
+    }
+    (void)has_stragler;
+    (void)stragler;
+
+    std::cout << "First: Creating pairs" << std::endl;
+    std::vector<std::pair<int, int> > pairs = create_pairs(nums);
+
+    std::cout << "Second: Swapping pairs" << std::endl;
+    swap_pairs(pairs);
+    
+    std::cout << "Third: Sorting pairs recursively" << std::endl;
+    recursive_sort_larger_element(pairs);
+}
+
 int main(int ac, char **av)
 {
+    // std::cout << jacobsthal(9) << std::endl;
     if(ac < 2)
     {
         std::cerr << "Usage: ./PmergeMe [num1] [num2] [num3] ..." << std::endl;
@@ -70,7 +97,8 @@ int main(int ac, char **av)
 
     PmergeMe(num_vector);
 
-    print(num_vector);
+    // print(num_vector);
+
 
     return 0;
 }
