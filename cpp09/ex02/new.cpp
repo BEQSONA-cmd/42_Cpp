6e73003d8cb3fefd988b0fd80e079817735f4a26
Beqa <tvildiani2001@gmail.com>
2024-12-02 15:59:06 +0100

    cpp09

--- cpp09/ex02/new.cpp
+++ cpp09/ex02/new.cpp
@@ -1,3 +1,20 @@
-#include <iostream>
-#include <vector>
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
 
+// int main(void)
+// {
+//     std::vector<int> nums = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+//     int a = 4;
+//     int b = 5;
+
+//     std::vector<int>::iterator low = std::lower_bound(nums.begin(), nums.end(), a);
+//     std::vector<int>::iterator up = std::upper_bound(nums.begin(), nums.end(), b);
+
+//     std::cout << "lower_bound for a: " << *low << std::endl;
+//     std::cout << "upper_bound for b: " << *up << std::endl;
+
+
+//     return 0;
+// }
